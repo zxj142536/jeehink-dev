052195f27a3f2508da00ccd9a2ecd09929882a0c
ext.zhaoxiaojie3 <1425124470@qq.com>
2022-10-26 15:50:00 +0800

    holle1

---
+++ src/main/java/com/example/spingbook/SpingbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spingbook/SpingbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
